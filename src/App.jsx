5cd36618edb5265b2e0d7e99f1f0679789b56bed
Moises Hdz <moy.hdz.g@gmail.com>
2023-05-20 20:57:37 -0700

    actualizando y completando un poco la Página de home

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import './components/styles.module.css'
 import './App.css'
-
+import {Container,Col,Row} from 'react-bootstrap'
+import moyPic from './imgs/moyPic.png'
 
 function App() {
 
@@ -11,10 +12,22 @@ function App() {
     
     <div className="custom-bg">
       <header><Header/></header>
-      <div>Home</div>
+        <Container>
+          <Row>
+            <Col className="descHome">
+              <p>
+              Ing. Electrómecánico trabajando en la industria Maquiladora Fronteriza de México! Tengo alrededor de 20 años de experiencia en esa industria!. Ahora con mis deseos de aprender y conocer otras áreas y desarrollar un poco de aire creativo decidí empezar un nuevo camino para aprender a programar y conocer el mundo de la tecnólogía FullStack Development! 
+              </p>          
+            </Col>
+            <Col>
+              <img className="moyPic" src={moyPic} />
+            </Col>
+          </Row>
+        </Container>
+        
       <Footer/>
     </div>
-    
+
     )
 }
 
